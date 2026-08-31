8dd78d4f8dac5a88a8645d1377c3bc03cc8a5f87
Denis <abramovdenis888@gmail.com>
2026-08-31 12:35:37 +0200

    create time line

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -4,7 +4,7 @@ export default function Process() {
   return (
     <section>
       <div className="container">
-        <div className="section-header reveal">
+        <div className="section-header no-divider reveal">
           <h2>So arbeiten wir mit Ihnen.</h2>
           <span className="counter mono">02</span>
         </div>
